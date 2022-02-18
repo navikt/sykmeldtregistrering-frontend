d5debceab31602b2527d1f85bf182bc0a3a829f6
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-02-18 07:06:11 +0100

    r setter basePath i next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false
+  swcMinify: false,
+  basePath: '/arbeid/registrering-ny'
 }
 
 module.exports = nextConfig
