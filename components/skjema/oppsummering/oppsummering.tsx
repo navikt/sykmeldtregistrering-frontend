32da79ec6ad6ef8fd5e4c4d7ea462ce1fe664bf3
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2022-02-25 14:48:59 +0100

    f Laget (norske) tekster for oppsummeringen

--- components/skjema/oppsummering/oppsummering.tsx
+++ components/skjema/oppsummering/oppsummering.tsx
@@ -3,28 +3,45 @@
 import { GuidePanel, Heading, Ingress, Table } from '@navikt/ds-react';
 import OppsummeringSvg from './oppsummering-svg';
 import { SkjemaState } from '../../../pages/skjema/[side]';
+import lagHentTekstForSprak, {Tekster} from "../../../lib/lag-hent-tekst-for-sprak";
+import useSprak from "../../../hooks/useSprak";
+import {Jobbsituasjon} from "../din-situasjon";
+
+const TEKSTER: Tekster<string> = {
+    nb: {
+        header: "Er opplysningene riktige?",
+        ingress: "Her er opplysningene vi har registrert om deg.",
+        ikkeIJobbSisteAaret:
+            `Ifølge Arbeidsgiver- og arbeidstakerregisteret har du ikke vært i jobb i løpet av det siste året. 
+             Hvis det er feil, er det likevel viktig at du fullfører registreringen. Du kan gi riktig informasjon senere til NAV.`,
+        situasjon: "Situasjon",
+        sisteStilling: "Siste stilling",
+        hoyesteFullforteUtdanning: "Høyeste fullførte utdanning",
+    },
+};
+
 
 const Oppsummering = (props: SkjemaState) => {
+    const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
+
     return (
         <>
-            <Heading size={'medium'}>Er opplysningene riktige?</Heading>
-            <Ingress>Her er opplysningene vi har registrert om deg.</Ingress>
+            <Heading size={'medium'}>{tekst('header')}</Heading>
+            <Ingress>{tekst('ingress')}</Ingress>
             <GuidePanel poster illustration={<OppsummeringSvg />}>
-                Ifølge Arbeidsgiver- og arbeidstakerregisteret har du ikke vært i jobb i løpet av det siste året. Hvis
-                det er feil, er det likevel viktig at du fullfører registreringen. Du kan gi riktig informasjon senere
-                til NAV.
+                {tekst('ikkeIJobbSisteAaret')}
                 <Table>
                     <Table.Body>
                         <Table.Row>
-                            <Table.HeaderCell scope="row">Situasjon</Table.HeaderCell>
+                            <Table.HeaderCell scope="row">{tekst('situasjon')}</Table.HeaderCell>
                             <Table.DataCell>{props.dinSituasjon}</Table.DataCell>
                         </Table.Row>
                         <Table.Row>
-                            <Table.HeaderCell scope="row">Siste stilling</Table.HeaderCell>
+                            <Table.HeaderCell scope="row">{tekst('sisteStilling')}</Table.HeaderCell>
                             <Table.DataCell>{props.sisteJobb}</Table.DataCell>
                         </Table.Row>
                         <Table.Row>
-                            <Table.HeaderCell scope="row">Høyeste fullførte utdanning</Table.HeaderCell>
+                            <Table.HeaderCell scope="row">{tekst('hoyesteFullforteUtdanning')}</Table.HeaderCell>
                             <Table.DataCell>{props.utdanning}</Table.DataCell>
                         </Table.Row>
                     </Table.Body>
