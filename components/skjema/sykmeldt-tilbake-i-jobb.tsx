c50939d5825153e8c6266a38d1d2f6c3ea534211
ane.sporastoyl <ane.sporastoyl@bekk.no>
2022-02-28 11:44:33 +0100

    Legg til spørsmål om  når man er tilbake i jobb

---
+++ components/skjema/sykmeldt-tilbake-i-jobb.tsx
@@ -0,0 +1,41 @@
+import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-sprak';
+import useSprak from '../../hooks/useSprak';
+import { Heading } from '@navikt/ds-react';
+import RadioGruppe from '../radio-gruppe/radio-gruppe';
+import { SkjemaKomponentProps } from './skjema-felleskomponenter';
+
+const TEKSTER: Tekster<string> = {
+    nb: {
+        tittel: 'Tror du at du kommer tilbake i jobb før du har vært sykmeldt i 52 uker?',
+        jaFullStilling: 'Ja, i full stilling',
+        jaRedusertStilling: 'Ja, i redusert stilling',
+        usikker: 'Usikker',
+        nei: 'Nei',
+    },
+};
+
+const TilbakeIJobb = (props: SkjemaKomponentProps) => {
+    const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
+    const { onChange, valgt } = props;
+
+    const valg = [
+        { tekst: tekst('jaFullStilling'), value: 'jaFullStilling' },
+        { tekst: tekst('jaRedusertStilling'), value: 'jaRedusertStilling' },
+        { tekst: tekst('usikker'), value: 'usikker' },
+        { tekst: tekst('nei'), value: 'nei' },
+    ];
+
+    return (
+        <>
+            <Heading spacing size={'large'} level="1">
+                {tekst('tittel')}
+            </Heading>
+
+            <form className="mbl">
+                <RadioGruppe valg={valg} onSelect={onChange} valgt={valgt} />
+            </form>
+        </>
+    );
+};
+
+export default TilbakeIJobb;
