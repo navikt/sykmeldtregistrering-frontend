ec3204b6c2f77b8dbe865ee789d41abdb28b2842
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-01 07:53:12 +0100

    r konfigurerer amplitude

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import NextApp, { AppContext, AppProps } from "next/app";
 import { onLanguageSelect } from "@navikt/nav-dekoratoren-moduler";
+import { AmplitudeProvider } from '../contexts/amplitude-context'
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks')
@@ -11,7 +12,11 @@ function MyApp({ Component, pageProps, router }: AppProps) {
       router.push(router.asPath, router.asPath, { locale })
   );
 
-  return <Component {...pageProps} />
+  return (
+    <AmplitudeProvider>
+      <Component {...pageProps} />
+    </AmplitudeProvider>
+  )
 }
 
 MyApp.getInitialProps = async function getInitialProps(context: AppContext) {
