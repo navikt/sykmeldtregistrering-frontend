b241e29d30960d4e2564d0fe6c0f648d5f57c1a6
Jostein Holje <jostein.holje@nav.no>
2022-03-02 12:38:24 +0100

    r Slett msw

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,20 @@
-import '../styles/globals.css'
-import NextApp, { AppContext, AppProps } from "next/app";
-import { onLanguageSelect } from "@navikt/nav-dekoratoren-moduler";
-import { AmplitudeProvider } from '../contexts/amplitude-context'
-
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-  require('../mocks')
-}
+import '../styles/globals.css';
+import NextApp, { AppContext, AppProps } from 'next/app';
+import { onLanguageSelect } from '@navikt/nav-dekoratoren-moduler';
+import { AmplitudeProvider } from '../contexts/amplitude-context';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  onLanguageSelect(({ locale }) =>
-      router.push(router.asPath, router.asPath, { locale })
-  );
+    onLanguageSelect(({ locale }) => router.push(router.asPath, router.asPath, { locale }));
 
-  return (
-    <AmplitudeProvider>
-      <Component {...pageProps} />
-    </AmplitudeProvider>
-  )
+    return (
+        <AmplitudeProvider>
+            <Component {...pageProps} />
+        </AmplitudeProvider>
+    );
 }
 
 MyApp.getInitialProps = async function getInitialProps(context: AppContext) {
-  return NextApp.getInitialProps(context);
-}
+    return NextApp.getInitialProps(context);
+};
 
-export default MyApp
-
+export default MyApp;
