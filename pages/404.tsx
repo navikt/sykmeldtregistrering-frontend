b00723ced6e0ef604ea7cbd4d83695ddd8909d3a
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-04 06:59:43 +0100

    f legger til egen 404-side

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ContentContainer } from '@navikt/ds-react';
+
+function NotFound() {
+    return (
+        <ContentContainer>
+            <div>Fant ikke siden</div>
+        </ContentContainer>
+    );
+}
+
+export default NotFound;
