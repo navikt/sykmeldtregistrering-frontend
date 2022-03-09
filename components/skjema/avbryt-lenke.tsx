157d32f7e397e5503c234ec6a1cbe91b36389e27
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-09 07:33:17 +0100

    f legger til avbryt modal

--- components/skjema/avbryt-lenke.tsx
+++ components/skjema/avbryt-lenke.tsx
@@ -1,20 +1,62 @@
+import { BodyLong, Button, Heading, Link, Modal } from '@navikt/ds-react';
+import { useState } from 'react';
+
 import skjemaStyles from '../../styles/skjema.module.css';
-import { Link } from '@navikt/ds-react';
 import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-sprak';
 import useSprak from '../../hooks/useSprak';
 
 const TEKSTER: Tekster<string> = {
     nb: {
         avbryt: 'Avbryt registreringen',
+        erDuSikker: 'Er du sikker på at du vil avbryte registreringen?',
+        knappJa: 'Ja, avbryt',
+        knappNei: 'Nei',
+        ariaLabel: 'Bekreft at du ønsker å avbryte registreringen',
+    },
+    en: {
+        avbryt: 'Cancel the registration',
+        erDuSikker: 'Are you sure you want to cancel the registration?',
+        knappJa: 'Yes, cancel',
+        knappNei: 'No',
+        ariaLabel: 'Confirm the cancellation',
     },
 };
 
 const Avbryt = () => {
+    const [open, setOpen] = useState(false);
     const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
+
     return (
-        <div className={skjemaStyles.taCenter}>
-            <Link href="#">{tekst('avbryt')}</Link>
-        </div>
+        <>
+            <Modal
+                shouldCloseOnOverlayClick={false}
+                open={open}
+                onClose={() => setOpen(false)}
+                aria-label={tekst('ariaLabel')}
+            >
+                <Modal.Content>
+                    <Heading spacing level="1" size="large">
+                        {tekst('avbryt')}
+                    </Heading>
+                    <Heading spacing level="2" size="medium">
+                        {tekst('erDuSikker')}
+                    </Heading>
+                    <BodyLong spacing className={skjemaStyles.spaceEvenly}>
+                        <Button variant="secondary" onClick={() => setOpen(false)} className={skjemaStyles.w10}>
+                            {tekst('knappJa')}
+                        </Button>
+                        <Button variant="secondary" onClick={() => setOpen(false)} className={skjemaStyles.w10}>
+                            {tekst('knappNei')}
+                        </Button>
+                    </BodyLong>
+                </Modal.Content>
+            </Modal>
+            <div className={skjemaStyles.taCenter}>
+                <Link href="#" onClick={() => setOpen(true)}>
+                    {tekst('avbryt')}
+                </Link>
+            </div>
+        </>
     );
 };
 
