8f0663b349290f7bc345ab73472f765c6c3449ba
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-10 07:37:00 +0100

    r skriver inn kryssklassifisering med pam-janzz url i stedet

--- pages/api/kryssklassifiser.ts
+++ pages/api/kryssklassifiser.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 async function kryssklassifiser(req: NextApiRequest, res: NextApiResponse<string>) {
     const styrkKode = req.query.styrkKode;
-    const url = `${process.env.KRYSSKLASSIFISER_URL}?kodeForOversetting=${styrkKode}`;
+    const url = `${process.env.PAM_JANZZ_URL}/kryssklassifiserMedKonsept?kodeForOversetting=${styrkKode}`;
     const response = await fetch(url);
     const json = await response.json();
     res.status(200).json(json);
