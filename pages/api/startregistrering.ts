2c6675180b40b537bdf7cf84aa01046a99d19b49
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-10 14:14:44 +0100

    r legger opp rett bruk av stratregistrering

--- pages/api/startregistrering.ts
+++ pages/api/startregistrering.ts
@@ -1,10 +1,35 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiHandler } from 'next';
+import { nanoid } from 'nanoid';
 
-async function startRegistrering(req: NextApiRequest, res: NextApiResponse<string>) {
-    const url = process.env.START_REGISTRERING_URL;
-    const response = await fetch(url!);
-    const json = await response.json();
-    res.status(200).json(json);
-}
+const startRegistreringUrl = `${process.env.START_REGISTRERING_URL}`;
 
-export default startRegistrering;
+const startHandler: NextApiHandler = async (req, res) => {
+    const idtoken = req.cookies['selvbetjening-idtoken'];
+    const callId = nanoid();
+    try {
+        const response = await fetch(startRegistreringUrl.toString(), {
+            headers: {
+                cookie: `selvbetjening-idtoken=${idtoken}`,
+                'Nav-Consumer-Id': 'poa-arbeidssokerregistrering',
+                'Nav-Call-Id': callId,
+            },
+        }).then(async (apiResponse) => {
+            const contentType = apiResponse.headers.get('content-type');
+            if (!contentType || !contentType.includes('application/json')) {
+                throw new TypeError(
+                    `Fikk ikke JSON fra startregistrering API (callId ${callId}). Body: ${await apiResponse.text()}.`
+                );
+            }
+
+            return apiResponse.json();
+        });
+
+        return res.json(response);
+    } catch (error) {
+        console.error(`Kall mot startregistrering API (callId: ${callId}) feilet. Feilmelding: ${error}`);
+
+        res.status(500).end(`Noe gikk galt (callId: ${callId})`);
+    }
+};
+
+export default startHandler;
