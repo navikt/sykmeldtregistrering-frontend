7c9bca39ce9c9dbca274171f451f1919a9cb52c0
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-11 13:26:10 +0100

    r legger til optional for å støtte POST

--- lib/next-api-handler.ts
+++ lib/next-api-handler.ts
@@ -1,31 +1,43 @@
 import { NextApiHandler } from 'next';
 import { nanoid } from 'nanoid';
 
-const lagApiHandlerMedAuthHeaders: (url: string) => NextApiHandler = (url: string) => async (req, res) => {
-    const idtoken = req.cookies['selvbetjening-idtoken'];
-    const callId = nanoid();
-    try {
-        const response = await fetch(url, {
-            headers: {
-                cookie: `selvbetjening-idtoken=${idtoken}`,
-                'Nav-Consumer-Id': 'poa-arbeidssokerregistrering',
-                'Nav-Call-Id': callId,
-            },
-        }).then(async (apiResponse) => {
-            const contentType = apiResponse.headers.get('content-type');
-            if (!contentType || !contentType.includes('application/json')) {
-                throw new TypeError(`Fikk ikke JSON fra ${url} (callId ${callId}). Body: ${await apiResponse.text()}.`);
-            }
+type Method = 'POST';
 
-            return apiResponse.json();
-        });
+interface Options {
+    method: Method;
+    body: Object;
+}
 
-        return res.json(response);
-    } catch (error) {
-        console.error(`Kall mot ${url} (callId: ${callId}) feilet. Feilmelding: ${error}`);
+const lagApiHandlerMedAuthHeaders: (url: string, options?: Options) => NextApiHandler =
+    (url: string, options?: Options) => async (req, res) => {
+        const idtoken = req.cookies['selvbetjening-idtoken'];
+        const callId = nanoid();
+        try {
+            const response = await fetch(url, {
+                method: options?.method || 'GET',
+                body: options ? JSON.stringify(options.body) : '',
+                headers: {
+                    cookie: `selvbetjening-idtoken=${idtoken}`,
+                    'Nav-Consumer-Id': 'poa-arbeidssokerregistrering',
+                    'Nav-Call-Id': callId,
+                },
+            }).then(async (apiResponse) => {
+                const contentType = apiResponse.headers.get('content-type');
+                if (!contentType || !contentType.includes('application/json')) {
+                    throw new TypeError(
+                        `Fikk ikke JSON fra ${url} (callId ${callId}). Body: ${await apiResponse.text()}.`
+                    );
+                }
 
-        res.status(500).end(`Noe gikk galt (callId: ${callId})`);
-    }
-};
+                return apiResponse.json();
+            });
+
+            return res.json(response);
+        } catch (error) {
+            console.error(`Kall mot ${url} (callId: ${callId}) feilet. Feilmelding: ${error}`);
+
+            res.status(500).end(`Noe gikk galt (callId: ${callId})`);
+        }
+    };
 
 export default lagApiHandlerMedAuthHeaders;
