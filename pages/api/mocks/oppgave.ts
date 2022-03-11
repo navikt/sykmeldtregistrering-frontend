356748b63ab4e8b78fdf199082fa564196bfd949
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-11 08:47:26 +0100

    f legger til endepunkter for opprett oppgave

---
+++ pages/api/mocks/oppgave.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const opprettOppgave = (req: NextApiRequest, res: NextApiResponse): void => {
+    res.status(200).json({
+        id: 308030803,
+        tildeltEnhetsnr: 1503,
+        data: '',
+        response: '',
+    });
+};
+
+export default opprettOppgave;
