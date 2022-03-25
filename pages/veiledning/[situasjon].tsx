766cce96fab3b828e5f9696353156679188f0311
ane.sporastoyl <ane.sporastoyl@bekk.no>
2022-03-25 10:45:37 +0100

    r Ha oppgavekvittering som komponent i stedet for egen side

--- pages/veiledning/[situasjon].tsx
+++ pages/veiledning/[situasjon].tsx
@@ -1,10 +1,24 @@
 import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-sprak';
 import useSprak from '../../hooks/useSprak';
 import { useRouter } from 'next/router';
-import { useCallback } from 'react';
-import { NextApiResponse } from 'next';
+import { useCallback, useState } from 'react';
 import { fetcher as api } from '../../lib/api-utils';
-import { BodyShort, Button, Heading, Panel } from '@navikt/ds-react';
+import {
+    Alert,
+    BodyShort,
+    Button,
+    Cell,
+    ContentContainer,
+    Detail,
+    Grid,
+    GuidePanel,
+    Heading,
+    Label,
+    Link,
+    Panel,
+} from '@navikt/ds-react';
+import virkedager from '@alheimsins/virkedager';
+import { ExternalLink } from '@navikt/ds-icons';
 
 export type Situasjon = 'utvandret' | 'mangler-arbeidstillatelse';
 
@@ -16,6 +30,23 @@ const TEKSTER: Tekster<string> = {
         body2: 'Dette gjør at du ikke kan registrere deg som arbeidssøker på nett.',
         kontaktOss: 'Kontakt oss, så hjelper vi deg videre.',
         kontaktKnapp: 'Ta kontakt',
+        mottatt: 'Henvendelse mottatt',
+        viktig: 'Viktig:',
+        kontakterDegInnen: 'Vi kontakter deg innen utgangen av ',
+        kontaktopplysningerOppdatert: 'Pass på at kontaktopplysningene dine er oppdatert, ellers kan vi ikke nå deg.',
+        tlfHosKRR: 'Telefonnummer lagret hos Kontakt- og reservasjonsregisteret',
+        kildeKRR: 'Kilde: Kontakt- og reservasjonsregisteret',
+        tlfHosNAV: 'Telefonnummer lagret hos NAV',
+        kildeNAV: 'Kilde: NAV',
+        endreOpplysninger: 'Endre opplysninger',
+    },
+    en: {
+        mottatt: 'Request received',
+        viktig: 'Important:',
+        kontakterDegInnen: 'We will contact you before the end of ',
+        kontaktopplysningerOppdatert: 'Please make sure your contact details are updated.',
+        endreOpplysninger: 'Change contact details',
+        //TODO: Oversette alle tekster
     },
 };
 
@@ -23,30 +54,94 @@ const KontaktVeileder = (props: { situasjon: Situasjon }) => {
     const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
     const router = useRouter();
 
+    const idag = new Date();
+    const nesteVirkedag = virkedager(idag, 2);
+    const [oppgaveOpprettet, settOppgaveOpprettet] = useState<boolean>(false);
+
     const opprettOppgave = useCallback(async () => {
         try {
             const oppgaveType = props.situasjon === 'utvandret' ? 'UTVANDRET' : 'OPPHOLDSTILLATELSE';
 
-            const response: NextApiResponse<any> = await api('/api/oppgave', {
+            await api('/api/oppgave', {
                 method: 'post',
                 body: JSON.stringify({ oppgaveType: oppgaveType }),
             });
-
-            return router.push('/veiledning/kvittering/');
+            settOppgaveOpprettet(true);
         } catch (e) {}
-    }, [props.situasjon, router]);
+    }, [props.situasjon]);
+
+    if (oppgaveOpprettet) {
+        return <HenvendelseMottatt />;
+    } else
+        return (
+            <Panel border>
+                <Heading size="medium" spacing={true}>
+                    {tekst('heading')}
+                </Heading>
+                <BodyShort>
+                    {tekst(props.situasjon === 'utvandret' ? 'utvandretBody1' : 'manglerArbeidstillatelseBody1')}
+                </BodyShort>
+                <BodyShort spacing>{tekst('body2')}</BodyShort>
+                <BodyShort spacing>{tekst('kontaktOss')}</BodyShort>
+                <Button onClick={opprettOppgave}>{tekst('kontaktKnapp')}</Button>
+            </Panel>
+        );
+};
+
+const HenvendelseMottatt = () => {
+    const sprak = useSprak();
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+    return (
+        <ContentContainer>
+            <GuidePanel poster>
+                <Grid>
+                    <Cell xs={12}>
+                        <Alert variant={'success'}>{tekst('mottatt')}</Alert>
+                    </Cell>
+                    <Cell xs={12}>
+                        <Heading spacing size={'small'}>
+                            {tekst('viktig')}
+                        </Heading>
+                        <BodyShort spacing>
+                            {tekst('kontakterDegInnen')}
+                            xx.xx.xxxx.
+                            {tekst('kontaktopplysningerOppdatert')}
+                        </BodyShort>
+                    </Cell>
+                    <Cell xs={12}>
+                        <Kontaktinformasjon kilde="KRR" tlfnr="XXXXXXXXX" />
+                    </Cell>
+                    <Cell xs={12}>
+                        <Kontaktinformasjon kilde="NAV" tlfnr="XXXXXXXXX" />
+                    </Cell>
+                    <Cell xs={12}>
+                        <Link href="https://www.nav.no/person/personopplysninger/#kontaktinformasjon">
+                            {tekst('endreOpplysninger')}
+                            <ExternalLink />
+                        </Link>
+                    </Cell>
+                </Grid>
+            </GuidePanel>
+        </ContentContainer>
+    );
+};
+
+type KontaktinfoProps = { kilde: Kilde; tlfnr: string };
+type Kilde = 'KRR' | 'NAV';
+
+const Kontaktinformasjon = (props: KontaktinfoProps) => {
+    const sprak = useSprak();
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
+    const idag = new Date();
+    const nesteVirkedag = virkedager(idag, 2);
 
     return (
         <Panel border>
-            <Heading size="medium" spacing={true}>
-                {tekst('heading')}
+            <Heading spacing size={'small'}>
+                {tekst(`tlfHos${props.kilde}`)}
             </Heading>
-            <BodyShort>
-                {tekst(props.situasjon === 'utvandret' ? 'utvandretBody1' : 'manglerArbeidstillatelseBody1')}
-            </BodyShort>
-            <BodyShort spacing>{tekst('body2')}</BodyShort>
-            <BodyShort spacing>{tekst('kontaktOss')}</BodyShort>
-            <Button onClick={opprettOppgave}>{tekst('kontaktKnapp')}</Button>
+            <Label>props.tlfnr</Label>
+            <Detail size={'small'}>{tekst(`kilde${props.kilde}`)}</Detail>
         </Panel>
     );
 };
