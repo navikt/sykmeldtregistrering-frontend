dbff5cf4e10c1ba3b9e0557845ef5255c5bf0459
ane.sporastoyl <ane.sporastoyl@bekk.no>
2022-03-28 09:44:30 +0200

    f Legg til helptext for Ingen kontaktopplysninger
    La til display flex for at hjelpetekstknappen (liten runding med spørsmålstegn inni) skulle havne ved siden av teksten og ikke under.

--- components/kontaktinformasjon.tsx
+++ components/kontaktinformasjon.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Cell, Detail, Heading, Label, Panel } from '@navikt/ds-react';
+import { Alert, Cell, Detail, Heading, HelpText, Label, Panel } from '@navikt/ds-react';
 import useSprak from '../hooks/useSprak';
 import lagHentTekstForSprak, { Tekster } from '../lib/lag-hent-tekst-for-sprak';
 
@@ -11,25 +11,36 @@ const TEKSTER: Tekster<string> = {
         tlfHosNAV: 'Telefonnummer lagret hos NAV',
         kildeNAV: 'Kilde: NAV',
         endreOpplysninger: 'Endre opplysninger',
+        ingenOpplysninger: 'Ingen kontaktopplysninger funnet!',
+        hjelpetekst: 'Pass på at kontaktopplysningene dine er oppdatert, ellers kan vi ikke nå deg.',
     },
     en: {
         tlfHosKRR: 'Phone number registered with the common contact register',
         kildeKRR: 'Source: The common contact register',
         tlfHosNAV: 'Phone number registered with NAV',
         kildeNAV: 'Source: NAV',
+        //TODO: Mangler oversettelse for ingenOpplysninger
         endreOpplysninger: 'Change contact details',
+        hjelpetekst: 'Please make sure your contact details are updated or we will be unable to reach you.',
     },
 };
 
 export const Kontaktinformasjon = (props: { kontaktinfo: Kontaktinfo }) => {
     const { telefonnummerKRR, telefonnummerNAV } = props.kontaktinfo;
     const manglerKontaktinfo = telefonnummerKRR === undefined && telefonnummerNAV === undefined;
+    const sprak = useSprak();
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
 
     if (manglerKontaktinfo) {
         //TODO: Riktig visning av feilmelding for manglende kontaktinfo
         return (
             <Cell xs={12}>
-                <BodyShort>Mangler kontaktinfo</BodyShort>{' '}
+                <Alert variant="error" inline>
+                    <div style={{ display: 'flex' }}>
+                        {tekst('ingenOpplysninger')}
+                        <HelpText>{tekst('hjelpetekst')}</HelpText>
+                    </div>
+                </Alert>
             </Cell>
         );
     } else {
