6755896623477047a26f04647cf266cdb8bc2507
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-03-31 07:17:48 +0200

    Legger til språkstøtte på tilbakeknappene

--- components/skjema/knapperad/tilbake-knapp.tsx
+++ components/skjema/knapperad/tilbake-knapp.tsx
@@ -1,13 +1,27 @@
 import { Button } from '@navikt/ds-react';
+
 import skjemaStyles from '../../../styles/skjema.module.css';
+import lagHentTekstForSprak, { Tekster } from '../../../lib/lag-hent-tekst-for-sprak';
+import useSprak from '../../../hooks/useSprak';
+
+const TEKSTER: Tekster<string> = {
+    nb: {
+        tilbake: 'Tilbake',
+    },
+    en: {
+        tilbake: 'Back',
+    },
+};
 
 export const TilbakeKnapp = (props: { onClick: () => void }) => {
+    const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
+
     return (
         <>
             <div className={skjemaStyles.taCenter}>
                 <Button variant="secondary" onClick={props.onClick}>
                     {' '}
-                    Tilbake{' '}
+                    {tekst('tilbake')}{' '}
                 </Button>
             </div>
         </>
--- components/skjema/tilbake-knapp.tsx
+++ components/skjema/tilbake-knapp.tsx
@@ -2,14 +2,29 @@ import { Back } from '@navikt/ds-icons';
 import { Link } from '@navikt/ds-react';
 import NextLink from 'next/link';
 
+import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-sprak';
+import useSprak from '../../hooks/useSprak';
+
 interface TilbakeKnappProps {
     href: string;
 }
+
+const TEKSTER: Tekster<string> = {
+    nb: {
+        tilbake: 'Tilbake',
+    },
+    en: {
+        tilbake: 'Back',
+    },
+};
+
 const TilbakeKnapp = (props: TilbakeKnappProps) => {
+    const tekst = lagHentTekstForSprak(TEKSTER, useSprak());
+
     return (
         <NextLink href={props.href} passHref={true}>
             <Link>
-                <Back /> Tilbake
+                <Back /> {tekst('tilbake')}
             </Link>
         </NextLink>
     );
