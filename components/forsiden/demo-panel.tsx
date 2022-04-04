19c37f68d73f8d220613bd2c9d27289922338f77
Jostein Holje <jostein.holje@nav.no>
2022-04-04 12:54:38 +0200

    f Knapper i demo-panelet

--- components/forsiden/demo-panel.tsx
+++ components/forsiden/demo-panel.tsx
@@ -22,6 +22,51 @@ function DemoPanel() {
                         <Button variant="secondary">Sykmeldt registrering</Button>
                     </NextLink>
                 </p>
+                <p>
+                    <NextLink href="/kvittering/" passHref>
+                        <Button variant="secondary">Kvittering</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/reaktivering" passHref>
+                        <Button variant="secondary">Reaktivering</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/kvittering-reaktivering/" passHref>
+                        <Button variant="secondary">Kvittering Reaktivering</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/veiledning/utvandret/" passHref>
+                        <Button variant="secondary">Utvandret</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/veiledning/mangler-arbeidstillatelse/" passHref>
+                        <Button variant="secondary">Mangler arbeidstillatelse</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/veiledning/allerede-registrert/" passHref>
+                        <Button variant="secondary">Allerede registrert</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/veiledning/sperret/" passHref>
+                        <Button variant="secondary">Sperret</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/vedlikehold/" passHref>
+                        <Button variant="secondary">Vedlikehold</Button>
+                    </NextLink>
+                </p>
+                <p>
+                    <NextLink href="/feil/" passHref>
+                        <Button variant="secondary">Feil</Button>
+                    </NextLink>
+                </p>
             </div>
         </Panel>
     );
