d137db98a1f3258b1e885263ca9f0a0560eadfac
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-04-06 06:39:50 +0200

    r legger til logging av stoppsituasjon

--- pages/veiledning/[situasjon].tsx
+++ pages/veiledning/[situasjon].tsx
@@ -1,8 +1,10 @@
+import { useCallback, useEffect, useState } from 'react';
+import { BodyShort, Button, Heading, Panel } from '@navikt/ds-react';
+
 import lagHentTekstForSprak, { Tekster } from '../../lib/lag-hent-tekst-for-sprak';
 import useSprak from '../../hooks/useSprak';
-import { useCallback, useState } from 'react';
+import { loggStoppsituasjon } from '../../lib/amplitude';
 import { fetcher as api } from '../../lib/api-utils';
-import { BodyShort, Button, Heading, Panel } from '@navikt/ds-react';
 import {
     KvitteringOppgaveIkkeOpprettet,
     KvitteringOppgaveOpprettet,
@@ -49,6 +51,12 @@ const KontaktVeileder = (props: { situasjon: Situasjon }) => {
         }
         settResponseMottatt(true);
     }, [props.situasjon]);
+
+    useEffect(() => {
+        loggStoppsituasjon({
+            situasjon: 'Arbeidssøkeren mangler arbeidstillatelse eller er utvandret',
+        });
+    }, []);
 
     if (responseMottatt) {
         return feil ? <KvitteringOppgaveIkkeOpprettet feil={feil} /> : <KvitteringOppgaveOpprettet />;
