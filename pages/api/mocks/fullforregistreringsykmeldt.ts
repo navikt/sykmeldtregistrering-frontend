0b6a6f6def9ab1a800841782d6be1ed282fdb168
Jostein Holje <jostein.holje@nav.no>
2022-04-08 14:13:27 +0200

    r mocks/fullforregistreringsykmeldt - returner 204

--- pages/api/mocks/fullforregistreringsykmeldt.ts
+++ pages/api/mocks/fullforregistreringsykmeldt.ts
@@ -4,7 +4,7 @@ const fullforHandler = (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const { body } = req;
         console.log('body', body);
-        return res.status(200).json({});
+        return res.status(204).end();
     } else {
         return res.status(405).end();
     }
