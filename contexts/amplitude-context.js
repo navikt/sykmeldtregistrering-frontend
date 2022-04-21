ebcbecb915d748c1a43ef2ec3a5573a79dbbd381
Jostein Holje <jostein.holje@nav.no>
2022-04-21 13:04:19 +0200

    r Bruk useConfig i amplitude-context

--- contexts/amplitude-context.js
+++ contexts/amplitude-context.js
@@ -1,22 +1,27 @@
-import { createContext, useContext, useEffect } from 'react'
-import { initAmplitude, logAmplitudeEvent } from '../lib/amplitude'
+import { createContext, useContext, useEffect } from 'react';
+import { initAmplitude, logAmplitudeEvent } from '../lib/amplitude';
+import { useConfig } from './config-context';
 
-const AmplitudeContext = createContext()
+const AmplitudeContext = createContext();
 
-function AmplitudeProvider ({ children }) {
-  useEffect(() => {
-    initAmplitude()
-  }, [])
+function AmplitudeProvider({ children }) {
+    const { amplitudeApiKey: apiKey, amplitudeEndPoint: apiEndpoint } = useConfig();
 
-  return <AmplitudeContext.Provider value={{ logAmplitudeEvent }}>{children}</AmplitudeContext.Provider>
+    useEffect(() => {
+        if (apiKey && apiEndpoint) {
+            initAmplitude({ apiKey, apiEndpoint });
+        }
+    }, [apiKey, apiEndpoint]);
+
+    return <AmplitudeContext.Provider value={{ logAmplitudeEvent }}>{children}</AmplitudeContext.Provider>;
 }
 
-function useAmplitude () {
-  const context = useContext(AmplitudeContext)
-  if (context === undefined) {
-    throw new Error('useAmplitude må brukes under en AmplitudeProvider')
-  }
-  return context
+function useAmplitude() {
+    const context = useContext(AmplitudeContext);
+    if (context === undefined) {
+        throw new Error('useAmplitude må brukes under en AmplitudeProvider');
+    }
+    return context;
 }
 
-export { AmplitudeProvider, useAmplitude }
+export { AmplitudeProvider, useAmplitude };
