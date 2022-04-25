c0eba86ddb66992fc937675565ae1733039f39d6
Jostein Holje <jostein.holje@nav.no>
2022-04-25 09:37:27 +0200

    f Ta imot optional errorHandler i lib/next-api-handler

--- lib/next-api-handler.ts
+++ lib/next-api-handler.ts
@@ -10,65 +10,70 @@ export const getHeaders = (idtoken: string, callId: string) => {
     };
 };
 
-export const lagApiPostHandlerMedAuthHeaders: (url: string) => NextApiHandler | undefined =
-    (url: string) => async (req, res) => {
-        if (req.method === 'POST') {
-            return lagApiHandlerMedAuthHeaders(url)(req, res);
-        } else {
-            res.status(405).end();
-        }
-    };
+export const lagApiPostHandlerMedAuthHeaders: (
+    url: string,
+    errorHandler?: (response: Response) => void
+) => NextApiHandler | undefined = (url: string, errorHandler) => async (req, res) => {
+    if (req.method === 'POST') {
+        return lagApiHandlerMedAuthHeaders(url, errorHandler)(req, res);
+    } else {
+        res.status(405).end();
+    }
+};
 
-interface ApiError extends Error {
+export interface ApiError extends Error {
     status?: number;
 }
 
-const lagApiHandlerMedAuthHeaders: (url: string) => NextApiHandler = (url: string) => async (req, res) => {
-    const idtoken = req.cookies['selvbetjening-idtoken'];
-    const callId = nanoid();
-    let body = null;
+const lagApiHandlerMedAuthHeaders: (url: string, errorHandler?: (response: Response) => void) => NextApiHandler =
+    (url: string, errorHandler) => async (req, res) => {
+        const idtoken = req.cookies['selvbetjening-idtoken'];
+        const callId = nanoid();
+        let body = null;
 
-    if (req.method === 'POST') {
-        body = req.body;
-    }
+        if (req.method === 'POST') {
+            body = req.body;
+        }
 
-    try {
-        const response = await fetch(url, {
-            method: req.method,
-            body,
-            headers: getHeaders(idtoken, callId),
-        }).then(async (apiResponse) => {
-            const contentType = apiResponse.headers.get('content-type');
-            const statusCode = apiResponse.status;
+        try {
+            const response = await fetch(url, {
+                method: req.method,
+                body,
+                headers: getHeaders(idtoken, callId),
+            }).then(async (apiResponse) => {
+                const contentType = apiResponse.headers.get('content-type');
+                const statusCode = apiResponse.status;
 
-            if (statusCode === 204) {
-                return apiResponse;
-            }
-
-            if (!apiResponse.ok) {
-                console.error(`apiResponse ikke ok callId - ${callId}`);
-                console.error(`contentType: ${contentType} callId - ${callId}`);
-                if (contentType && contentType.includes('application/json')) {
-                    return apiResponse.json();
-                } else {
-                    const error = new Error(apiResponse.statusText) as ApiError;
-                    error.status = apiResponse.status;
-                    throw error;
+                if (statusCode === 204) {
+                    return apiResponse;
                 }
-            }
 
-            if (!contentType || !contentType.includes('application/json')) {
-                throw new TypeError(`Fikk ikke JSON fra ${url} (callId ${callId}). Body: ${await apiResponse.text()}.`);
-            }
+                if (!apiResponse.ok) {
+                    console.error(`apiResponse ikke ok callId - ${callId}`);
+                    console.error(`contentType: ${contentType} callId - ${callId}`);
+                    if (typeof errorHandler === 'function') {
+                        return errorHandler(apiResponse);
+                    } else {
+                        const error = new Error(apiResponse.statusText) as ApiError;
+                        error.status = apiResponse.status;
+                        throw error;
+                    }
+                }
 
-            return apiResponse.json();
-        });
+                if (!contentType || !contentType.includes('application/json')) {
+                    throw new TypeError(
+                        `Fikk ikke JSON fra ${url} (callId ${callId}). Body: ${await apiResponse.text()}.`
+                    );
+                }
 
-        return res.json(response);
-    } catch (error) {
-        console.error(`Kall mot ${url} (callId: ${callId}) feilet. Feilmelding: ${error}`);
-        res.status((error as ApiError).status || 500).end(`Noe gikk galt (callId: ${callId})`);
-    }
-};
+                return apiResponse.json();
+            });
+
+            return res.json(response);
+        } catch (error) {
+            console.error(`Kall mot ${url} (callId: ${callId}) feilet. Feilmelding: ${error}`);
+            res.status((error as ApiError).status || 500).end(`Noe gikk galt (callId: ${callId})`);
+        }
+    };
 
 export default lagApiHandlerMedAuthHeaders;
