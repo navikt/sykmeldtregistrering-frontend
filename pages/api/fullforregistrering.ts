15016d4aaaac668a3996c38ce7c163c57e556ccc
Jostein Holje <jostein.holje@nav.no>
2022-04-25 09:38:07 +0200

    f api/fullforregistrering: send med errorHandler

--- pages/api/fullforregistrering.ts
+++ pages/api/fullforregistrering.ts
@@ -1,6 +1,18 @@
-import { lagApiPostHandlerMedAuthHeaders } from '../../lib/next-api-handler';
+import { ApiError, lagApiPostHandlerMedAuthHeaders } from '../../lib/next-api-handler';
 
 const fullforRegistreringUrl = `${process.env.FULLFOR_REGISTRERING_URL}`;
-const fullforHandler = lagApiPostHandlerMedAuthHeaders(fullforRegistreringUrl);
+
+const errorHandler = (response: Response) => {
+    const contentType = response.headers.get('content-type');
+    if (contentType && contentType.includes('application/json')) {
+        return response.json();
+    } else {
+        const error = new Error(response.statusText) as ApiError;
+        error.status = response.status;
+        throw error;
+    }
+};
+
+const fullforHandler = lagApiPostHandlerMedAuthHeaders(fullforRegistreringUrl, errorHandler);
 
 export default fullforHandler;
